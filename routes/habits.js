ff726c71b109fbf4a78c78613987304421dc2ca5
syedmjavaid <syedmjavaid@gmail.com>
2022-06-27 15:29:39 +0100

    Add docker commands, controller and model functions for habits

--- routes/habits.js
+++ routes/habits.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const habitsController = require("../controllers/habits.controllers");
 
 router.get("/api/habits", habitsController.index);
+router.get("/api/habits/:id", habitsController.show);
+router.post("/api/habits", habitsController.create);
+router.delete("/api/habits/:id", habitsController.destroy);
 
 module.exports = router;
