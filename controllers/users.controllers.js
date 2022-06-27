88ba798c6539c38c22cd50f127c685b790340983
Summira Hussain <summira508411@gmail.com>
2022-06-27 17:18:45 +0100

    bugs fixed

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -12,8 +12,8 @@ async function index(req, res) {
 async function show(req, res) {
     try {
         const user = await User.findById(req.params.id);
-        const habits = await user.habits;
-        res.status(200).json({ ...user, habits });
+        // const habits = await user.habits;
+        res.status(200).json(user);
     } catch (err) {
         res.status(500).send(err);
     };
