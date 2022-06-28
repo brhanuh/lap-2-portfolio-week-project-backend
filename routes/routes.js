f7e7878018dd1bae2d2b312c7439a6fd1d4640ec
syedmjavaid <syedmjavaid@gmail.com>
2022-06-28 09:20:30 +0100

    Update routes to only have one route file, habits routes function now

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const usersController = require("../controllers/users.controllers");
 const habitsController = require("../controllers/habits.controllers");
 
+// Habits routes
 router.get("/api/habits", habitsController.index);
 router.get("/api/habits/:id", habitsController.show);
 router.post("/api/habits", habitsController.create);
 router.delete("/api/habits/:id", habitsController.destroy);
 
+// Users routes
+router.get("/", usersController.index);
+router.get("/:id", usersController.show);
+router.post("/", usersController.create);
+router.delete("/:id", usersController.destroy);
 
 module.exports = router;
