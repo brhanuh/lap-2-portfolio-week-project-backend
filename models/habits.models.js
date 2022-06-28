e31cb33a4a06500e4d673d241b57ab0b50291658
syedmjavaid <syedmjavaid@gmail.com>
2022-06-28 16:00:59 +0100

    Add create and delete functions for habits, but they dont work properly

--- models/habits.models.js
+++ models/habits.models.js
@@ -6,13 +6,14 @@ const User = require("./users.models");
 module.exports = class Habit {
   constructor(data, user) {
     this.id = data.id;
-    this.habitName = data.habitName;
+    this.habit = data.habit;
     this.hours_per_day = data.hours_per_day;
     this.date = data.date;
-    this.user = {
-      name: data.username,
-      path: `/users/${data.user_id}`,
-    };
+    this.user_id = data.user_id;
+    // this.user = {
+    //   name: data.username,
+    //   path: `/${data.user_id}`,
+    // };
   }
 
   static get all() {
@@ -37,7 +38,25 @@ module.exports = class Habit {
         let habit = new Habit(habitData.rows[0]);
         resolve(habit);
       } catch (error) {
-        reject("Book not found");
+        reject("Habit not found");
+      }
+    });
+  }
+
+  static async create(habitData) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const { habit, hoursPerDay, date, username } = habitData;
+        console.log(habitData);
+        let user = await User.findOrCreateByName(username);
+        let newHabit = await db.query(
+          `INSERT INTO habits (habit, hours_per_day, date, user_id) VALUES ($1, $2, $3, $4) RETURNING *`,
+          [habit, hoursPerDay, date, user.id]
+        );
+
+        resolve(newHabit.rows[0]);
+      } catch (error) {
+        reject("Habit could not be created");
       }
     });
   }
